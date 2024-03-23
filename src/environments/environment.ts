9c2632c6fa845e264314dc924630f9adeeed843d
menso431 <menson.sundash@gmail.com>
2024-03-23 22:23:55 +1100

    modified uri

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 const WEB_URL = "https://mean-web.vercel.app/";//process.env.WEB_URL
 //const WEB_URL = "http://localhost:4200/";
-const API_URL = "https://mean-api-chi.vercel.app/";//process.env.API_URL
+const API_URL = "https://mean-api-three.vercel.app/";//process.env.API_URL
 //const API_URL = "http://localhost:8800/";
 export const environment = {
   production: false,
